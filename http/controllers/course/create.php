0ee2772eb6dc7ab161e408c058521a9793fd7d0e
Beamlak Aschalew <birrletej12@gmail.com>
2024-12-29 22:57:30 +0300

    Fixed error images and error css file

--- http/controllers/course/create.php
+++ http/controllers/course/create.php
@@ -3,6 +3,7 @@
 error_log("Starting create.php");
 
 try {
+    $r = 10 / 0;
     view("course/create/index.view.php", []);
 } catch (Throwable $e) {
     error_log("Caught exception: " . $e->getMessage());
