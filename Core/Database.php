9fca01338d7380f832072659530b4c1ce297b2b6
Beamlak Aschalew <birrletej12@gmail.com>
2025-01-19 00:09:54 +0300

    changed db config a bit

--- Core/Database.php
+++ Core/Database.php
@@ -9,7 +9,9 @@ class Database {
     public $connection;
     public $statement;
 
-    public function __construct($config, $username = 'root', $password = '') {
+    public function __construct($config) {
+        $username = $config['username'];
+        $password = $config['password'];
         $dsn = 'mysql:' . http_build_query($config, '', ';');
 
         $this->connection = new PDO($dsn, $username, $password, [
--- essentials/config.php
+++ essentials/config.php
@@ -1,9 +1,24 @@
 <?php
 
-return [
-    'database' => [
-        'host' => 'localhost',
-        'port' => 3306,
-        'dbname' => 'ewkethub_test'
-    ],
-];
+$environment = $_SERVER['SERVER_NAME'];
+
+if ($environment == 'localhost')
+    return [
+        'database' => [
+            'host' => 'localhost',
+            'port' => 3306,
+            'dbname' => 'ewkethub_test',
+            'username' => 'root',
+            'password' => ''
+        ],
+    ];
+else
+    return [
+        'database' => [
+            'host' => '91.204.209.13',
+            'port' => 3306,
+            'dbname' => 'beamlakdev_ewkethub_test',
+            'username' => 'beamlakdev_ewkethub',
+            'password' => 'ewkethubdbpassword'
+        ],
+    ];
