05e1402b75acf1ef196dd5a0721ec705691f1f23
Beamlak Aschalew <birrletej12@gmail.com>
2025-01-29 14:04:08 +0300

    Started edit course

--- views/partials/head.php
+++ views/partials/head.php
@@ -1,6 +1,7 @@
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<link rel="shortcut icon" href="<?= base_url('assets/images/favicon.ico') ?>" type="image/x-icon">
 <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 <link
