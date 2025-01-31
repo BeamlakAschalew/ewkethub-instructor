95e09b0ca4e0842745327b85fdd447f0a986630a
edom mulugeta <hailemariamedom@gmail.com>
2025-01-31 22:20:59 +0300

    session message to lesson

--- http/controllers/lesson/store.php
+++ http/controllers/lesson/store.php
@@ -7,31 +7,31 @@ use Core\Database;
 $config = require base_path("essentials/config.php");
 $database = new Database($config["database"]);
 
-$section = $database->query('SELECT id FROM section WHERE section_slug = :section_slug', [
-    'section_slug' => $data['section-slug']
-])->find();
-
 $course = $database->query('SELECT id, instructor_id FROM course WHERE course_slug = :course_slug', [
     'course_slug' => $data['course-slug']
 ])->find();
 
-if (!$section) {
+
+if (!$course) {
     abort([], 404);
 } else if (!owns($course['instructor_id'])) {
     abort([], 403);
-}
-
-$result = $database->query('INSERT INTO lesson (name, lesson_slug, description, section_id, video_file_path) VALUES (:lesson_name, :lesson_slug, :description, :section_id, :file_name)', [
-    'lesson_name' => $data['title'],
-    'lesson_slug' => $data['slug'],
-    'description' => $data['description'],
-    'section_id' => $section['id'],
-    'file_name' => $data['uploaded_video']
-]);
-
-if ($database->statement->rowCount() > 0) {
-    header("Location: /course/{$data['course-slug']}/section/{$data['section-slug']}/lesson/{$data['slug']}");
-    exit();
 } else {
-    echo "Error inserting data into the database.";
+    $courseId = $course['id'];
+    $result = $database->query('INSERT INTO section (section_name, section_slug, description, course_id) VALUES (:section_name, :section_slug, :description, :course_id)', [
+        'section_name' => $data['title'],
+        'section_slug' => $data['slug'],
+        'description' => $data['description'],
+        'course_id' => $courseId
+    ]);
+
+    if ($database->statement->rowCount() > 0) {
+        Core\Session::set('message', [
+            'type' => 'success',
+            'content' => 'Section created successfully.'
+        ]);
+        redirect("/course/{$data['course-slug']}");
+    } else {
+        echo "Error inserting data into the database.";
+    }
 }
